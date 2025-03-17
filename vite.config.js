56c6c4061c62a9b92481ae43937857e928350676
David Lima <antdavidlima@gmail.com>
2025-03-17 09:36:41 -0300

    Atualiza biblioteca de conversão para dashboard sankhya

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { plugin } from "web-dash-builder";
+import { convertToSankhyaBI } from "@insulino/vite-plugin-2sankhyabi";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), plugin()],
+  plugins: [react(), { ...convertToSankhyaBI(), apply: "build" }],
 });
